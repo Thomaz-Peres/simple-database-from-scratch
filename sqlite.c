e0da4b8c92cd71cc440cd1fc7a7332e66bc99e16
Thomaz <thomaz.peres21@hotmail.com>
2023-08-16 22:55:39 -0300

    finish part 4

--- sqlite.c
+++ sqlite.c
@@ -72,7 +72,8 @@ typedef enum {
     PREPARE_SUCCESS,
     PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT,
-    PREPARE_STRING_TOO_LONG
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_NEGATIVE_ID,
 } PrepareResult;
 
 
@@ -179,6 +180,10 @@ PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
     }
 
     int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+
     if (strlen(username) > COLUMN_USERNAME_SIZE) {
         return PREPARE_STRING_TOO_LONG;
     }
@@ -269,6 +274,9 @@ int main(int argc, char *argv[])
         {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
             case (PREPARE_STRING_TOO_LONG):
                 printf("String is too long.\n");
                 continue;
